4aeb2064daddc8157f5e9a614e5c7cfffc29f8b8
DarkMoonEnjoyer <theohaha04@gmail.com>
2026-05-11 08:48:00 +0800

    Updated with Breeze Integrated lol forgot to add it via php only composer

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,7 @@
-import "./bootstrap";
-import Chart from "chart.js/auto";
+import './bootstrap';
 
-window.Chart = Chart;
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
